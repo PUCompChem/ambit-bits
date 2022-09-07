f3468e125d23a20c2eadc7c905bba667361eb0f7
ntk73 <nick@uni-plovdiv.net>
2022-09-07 12:28:30 +0300

    Added functions for generating JnaRinchi code

--- src/main/java/pu/ambit/GenerateMoleculeCode.java
+++ src/main/java/pu/ambit/GenerateMoleculeCode.java
@@ -6,6 +6,7 @@ import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
 
+
 public class GenerateMoleculeCode {
 	public static String getCDKCodeForMolecule(String smiles) throws Exception {
 		return getCDKCodeForMolecule(smiles, false);
@@ -48,6 +49,49 @@ public class GenerateMoleculeCode {
 				sb.append(boPrefix + i + ".setIsAromatic(true);\n");
 			if (bo.isInRing())
 				sb.append(boPrefix + i + ".setIsRing(true);\n");
+				
+			sb.append(molVar + ".addBond(" + boPrefix + i + ");\n");
+		}
+		
+		return sb.toString();
+	}
+	
+	
+	public static String getJnaRinchiCodeForMolecule(String smiles) throws Exception {
+		return getJnaRinchiCodeForMolecule(smiles, false);
+	}
+	
+	public static String getJnaRinchiCodeForMolecule(String smiles, boolean kekulize) throws Exception {		
+		SmilesParser parser = new SmilesParser(SilentChemObjectBuilder.getInstance());
+        parser.kekulise(kekulize);
+        IAtomContainer mol = parser.parseSmiles(smiles);
+        return getJnaRinchiCodeForMolecule(mol, "ric", "a", "b");
+		
+	}
+	
+	public static String getJnaRinchiCodeForMolecule(IAtomContainer mol, String molVar, String atPrefix, String boPrefix) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("RinchiInputComponent " + molVar + " = new RinchiInputComponent();\n");
+		for (int i = 0; i < mol.getAtomCount(); i++) {
+			IAtom at = mol.getAtom(i);
+			String symbol = at.getSymbol();
+			sb.append("InchiAtom " + atPrefix + i + " = new InchiAtom(\"" + symbol +  "\");\n");
+			if (at.getFormalCharge() != null && at.getFormalCharge() != 0)
+				sb.append(atPrefix + i + ".setCharge(" + at.getFormalCharge() + ");\n");
+			if (at.getImplicitHydrogenCount() != null)
+				sb.append(atPrefix + i + ".setImplicitHydrogen(" + at.getImplicitHydrogenCount() + ");\n");			
+			sb.append(molVar + ".addAtom(" + atPrefix + i + ");\n");
+		}
+				
+		for (int i = 0; i < mol.getBondCount(); i++) {
+			IBond bo = mol.getBond(i);
+			int index0 = mol.indexOf(bo.getAtom(0));
+			int index1 = mol.indexOf(bo.getAtom(1));			
+			String bondOrderStr = bo.getOrder().toString();
+			
+			sb.append("InchiBond " + boPrefix + i + " = new InchiBond(" 
+					+ atPrefix + index0 + " ," + atPrefix + index1 + " ," + 
+					 "InchiBondType." + bondOrderStr + ");\n");
 				
 			sb.append(molVar + ".addBond(" + boPrefix + i + ");\n");
 		}
