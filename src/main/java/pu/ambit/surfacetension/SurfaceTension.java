1dc68c59807e9ffe240a128bfe2046470356e4b0
Nikolay Kochev <nick@uni-plovdiv.net>
2025-04-30 16:53:47 +0300

    Added new function setupGCMModel()

--- src/main/java/pu/ambit/surfacetension/SurfaceTension.java
+++ src/main/java/pu/ambit/surfacetension/SurfaceTension.java
@@ -1,6 +1,28 @@
 package pu.ambit.surfacetension;
 
+import java.io.File;
+
+import ambit2.groupcontribution.GroupContributionModel;
+import ambit2.groupcontribution.io.GCM2Json;
+
 public class SurfaceTension 
 {
+	public String eCohGCMModelFile = null;
+	GroupContributionModel gcmECoh = null; 
 	
+	public int setupGCMModel() throws Exception
+	{
+		GCM2Json g2j = new GCM2Json();
+		gcmECoh = g2j.loadFromJSON(new File(eCohGCMModelFile));
+		
+		if (!g2j.configErrors.isEmpty())
+		{	
+			System.out.println(g2j.getAllErrorsAsString());
+			return -1;
+		}	
+		else if (!g2j.configErrors.isEmpty())
+			System.out.println(g2j.getAllErrorsAsString());
+		
+		return 0;
+	}
 }
