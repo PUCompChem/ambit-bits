452faec196a0b1a7ca0629de89c01d260ad98208
Nikolay Kochev <nick@uni-plovdiv.net>
2025-09-18 16:47:33 +0300

    Implemented basic functionality for calculation of a surface tension based on two GCM models

--- src/main/java/pu/ambit/surfacetension/SurfaceTension.java
+++ src/main/java/pu/ambit/surfacetension/SurfaceTension.java
@@ -2,18 +2,29 @@ package pu.ambit.surfacetension;
 
 import java.io.File;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+
 import ambit2.groupcontribution.GroupContributionModel;
 import ambit2.groupcontribution.io.GCM2Json;
+import ambit2.smarts.SmartsHelper;
 
 public class SurfaceTension 
 {
-	public String eCohGCMModelFile = null;
+	private double stCoeff = 0.75;
+	
+	String eCohGCMModelFile = null;
 	GroupContributionModel gcmECoh = null; 
 	
-	public String molVolGCMModelFile = null;
+	String molVolGCMModelFile = null;
 	GroupContributionModel gcmMolVol = null;
 	
 	boolean flagGCMModelsStatus = false;
+	
+	public SurfaceTension(String eCohGCMModelFile, String molVolGCMModelFile) {
+		this.eCohGCMModelFile = eCohGCMModelFile;
+		this.molVolGCMModelFile = molVolGCMModelFile;
+	}
 	
 	public int setupGCMModels() throws Exception
 	{
@@ -35,7 +46,24 @@ public class SurfaceTension
 			return -1;
 		}
 		
+		gcmECoh.setAllowGroupRegistration(false);
+		gcmMolVol.setAllowGroupRegistration(false);
 		flagGCMModelsStatus = true;
 		return 0;
+	}
+	
+	public double calcSurfaceTenstion(String smiles) throws Exception 
+	{
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smiles, false);
+		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+		return calcSurfaceTenstion(mol);
+	}
+	
+	public double calcSurfaceTenstion(IAtomContainer mol) 
+	{					
+		double eCoh = gcmECoh.calcModelValue(mol);
+		double molVol = gcmMolVol.calcModelValue(mol);
+		double CED = eCoh/molVol;
+		return stCoeff * Math.pow(CED, 2.0/3);
 	}
 }
